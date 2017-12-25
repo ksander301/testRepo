eb388cb307dcc91c86357ac52cc98d7c8e98a2a9
ksander301 <sanek301@rambler.ru>
2017-12-25 21:59:45 +0400

    add second

---
+++ src/main/java/dao/impl/testDAOimpl.java
@@ -0,0 +1,17 @@
+package dao.impl;
+
+
+import dao.testDAO;
+
+public class testDAOimpl implements testDAO {
+
+    @Override
+    public void testMethod() {
+
+    }
+
+    @Override
+    public void testMethod2() {
+        //anything
+    }
+}
---
+++ src/main/java/dao/testDAO.java
@@ -0,0 +1,8 @@
+package dao;
+
+public interface  testDAO {
+void testMethod();
+    // enything code 2222
+
+    void testMethod2();
+}
